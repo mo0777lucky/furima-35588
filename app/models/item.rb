4d2f4c3a0125fbb3fe6c30f60c8918d77e8afe92
mo0777lucky <beautifulsea08264@gmail.com>
2021-05-11 17:19:38 +0900

    ActiveStorageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  validates :title, presence: true
+  validates :category_id, presence: true
+  validates :price, presence: true
+  validates :item_description, presence: true
+  validates :status_id, presence: true
+  validates :shipping_fee_id, presence: true
+  validates :shipping_area_id, presence: true
+  validates :shipping_date_id, presence: true
+  validates :user, presence: true
+
+  belongs_to :user
+  has_one :purchase_record
+  has_one_attached :image
 end
