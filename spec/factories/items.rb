4f4740e1e9c8a7f1975280b5f59cbee7a2815558
mo0777lucky <beautifulsea08264@gmail.com>
2021-05-13 13:44:20 +0900

    テストコード作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,19 @@
 FactoryBot.define do
   factory :item do
-    title {""}
-    category_id {Faker::Internet.free_email}
-    price {""}
-    item_description {""}
-    status_id {""}
-    shipping_fee_id {""}
-    shipping_area_id {""}
-    shipping_date_id {""}
-    user {""}
+    title {"帽子"}
+    category_id {2}
+    price {3500}
+    item_description {"あああああああああああああああああ"}
+    status_id {2}
+    shipping_fee_id {2}
+    shipping_area_id {2}
+    shipping_date_id {2}
+    user_id {2}
+    
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end   
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Item, type: :model do
     @item = FactoryBot.build(:item)
   end
 
-  describe 'ユーザー商品出品' do
+  describe '商品出品' do
     context '商品出品できるとき' do
       it '全ての情報を正しく入力すれば、商品出品できる' do
         expect(@item).to be_valid
@@ -13,114 +13,115 @@ RSpec.describe Item, type: :model do
     end
 
     context '商品出品できないとき' do
-      it 'imageが空では登録できない' do
-        @item.image = ''
+      it 'imageが空では出品できない' do
+        @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include ("Image can't be blank")
       end
 
-      it 'titleが空では登録できない' do
+      it 'titleが空では出品できない' do
         @item.title = ''
         @item.valid?
         expect(@item.errors.full_messages).to include ("Title can't be blank")
       end
 
-      it 'categoryが空では登録できない' do
-        @item.category = ''
+      it 'category_idが空では出品できない' do
+        @item.category_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include ("Category can't be blank")
       end
 
-      it 'priceが空では登録できない' do
+      it 'category_id選択が--では出品できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Category must be other than 1")
+      end
+
+      it 'priceが空では出品できない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include ("Price can't be blank")
       end
 
-      it 'item_descriptionが空では登録できない' do
-        @item.item_description = ''
+      it 'priceが全角数字では出品できないこと' do
+        @item.price = '１２３４'
         @item.valid?
-        expect(@item.errors.full_messages).to include ("Item_description  can't be blank")
+        expect(@item.errors.full_messages).to include ("Price is invalid")
       end
 
-      it 'statusが空では登録できない' do
-        @item.status = ''
+      it 'priceが¥300を下回ると出品できないこと' do
+        @item.price = 200
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Price is invalid")
+      end
+      
+      it 'priceが¥9,999,999を上回ると出品できないこと' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Price is invalid")
+      end
+
+      it 'item_descriptionが空では出品できない' do
+        @item.item_description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Item description can't be blank")
+      end
+
+      it 'status_idが空では出品できない' do
+        @item.status_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include ("Status can't be blank")
       end
 
-      it 'shipping_feeが空では登録できない' do
-        @item.shipping_fee = ''
+      it 'status_id選択が--では出品できない' do
+        @item.status_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include ("Shipping_fee can't be blank")
+        expect(@item.errors.full_messages).to include ("Status must be other than 1")
+      end
+
+
+      it 'shipping_fee_idが空では出品できない' do
+        @item.shipping_fee_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Shipping fee can't be blank")
       end
      
-      it 'shipping_areaが空では登録できない' do
-        @item.shipping_area = ''
+      it 'shipping_fee_id選択が--では出品できない' do
+        @item.shipping_fee_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include ("Shipping_area can't be blank")
+        expect(@item.errors.full_messages).to include ("Shipping fee must be other than 1")
       end
 
-      it 'shipping_dateが空では登録できない' do
-        @item.shipping_date = ''
+
+      it 'shipping_area_idが空では出品できない' do
+        @item.shipping_area_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include ("Shipping_date can't be blank")
+        expect(@item.errors.full_messages).to include ("Shipping area can't be blank")
       end
- 
-      it 'userが空では登録できない' do
-        @item.user = ''
+
+      it 'shipping_area_id選択が--は出品できない' do
+        @item.shipping_area_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include ("User can't be blank")
-      end
-      
-
-
-      it 'passwordが5文字以下であれば登録できないこと' do
-        @user.password = '12345'
-        @user.password_confirmation = '12345'
-        @user.valid?
-        expect(@user.errors.full_messages).to include ("Password is too short (minimum is 6 characters)")
+        expect(@item.errors.full_messages).to include ("Shipping area must be other than 1")
       end
 
-      it 'passwordが数字のみでは登録できないこと' do
-        @user.password = 123456
-        @user.password_confirmation = 123456
-        @user.valid?
-        expect(@user.errors.full_messages).to include ("Password is invalid")
-      end
-
-      it 'passwordが英字のみでは登録できないこと' do
-        @user.password = 'abcdef'
-        @user.password_confirmation = 'abcdef'
-        @user.valid?
-        expect(@user.errors.full_messages).to include ("Password is invalid")
-      end
-
-      it 'passwordが全角では登録できないこと' do
-        @item.password = 'AB１２３４'
-        @item.password_confirmation = 'AB１２３４'
+      it 'shipping_date_idが空では出品できない' do
+        @item.shipping_date_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include ("Password is invalid")
+        expect(@item.errors.full_messages).to include ("Shipping date can't be blank")
       end
 
-      it '重複したemailが存在する場合登録できないこと' do
-        @item.save
-        another_item = FactoryBot.build(:user, email: @user.email)
-        another_item.valid?
-        expect(another_item.errors.full_messages).to include ("Email has already been taken")
+      it 'shipping_date_id選択が--では出品できない' do
+        @item.shipping_date_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Shipping date must be other than 1")
       end
-
+  
+      it 'userが紐付いていないと保存できないこと' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
-
-
-#Price is invalid. Input half-width characters
-#Price is out of setting range
-#ログイン状態のユーザーのみ、商品出品ページへ遷移できること
-#ログアウト状態のユーザーは、商品出品ページへ遷移しようとすると、ログインページへ遷移すること
-#商品画像を1枚つけることが必須であること
-#販売価格は、¥300~¥9,999,999の間のみ保存可能であること
-#販売価格は半角数字のみ保存可能であること
-#入力された販売価格によって、販売手数料や販売利益の表示が変わること
-#必要な情報を適切に入力すると、商品情報がデータベースに保存されること
-#エラーハンドリングができていること（入力に問題がある状態で「出品する」ボタンが押された場合、情報は保存されず、出品ページに戻りエラーメッセージが表示されること）
-#エラーハンドリングの際、1つのエラーに対して同じエラーメッセージが重複して表示されないこと
