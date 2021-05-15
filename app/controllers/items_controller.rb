3c4648e05baaac282eddb66b523487b4babd1ef8
mo0777lucky <beautifulsea08264@gmail.com>
2021-05-15 21:36:56 +0900

    商品削除機能作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
  
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show,:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :set_message, only: [:edit, :update]
   
   def index
@@ -39,6 +39,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @items.destroy
+    redirect_to root_path
+  end
 
   private
   def item_params
