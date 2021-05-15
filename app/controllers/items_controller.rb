c6e18493edfa2e53f6434ae903ed212dc0cc017b
mo0777lucky <beautifulsea08264@gmail.com>
2021-05-15 21:55:09 +0900

    商品削除機能修正(2回目)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
  
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :set_message, only: [:edit, :update]
+  before_action :set_message, only: [:edit, :update, :destroy]
   
   def index
     @items = Item.order("created_at DESC")
