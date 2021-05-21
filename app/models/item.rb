10463e30e1c95a9e76fae9e49acfb791fb0f3df6
mo0777lucky <beautifulsea08264@gmail.com>
2021-05-21 11:16:36 +0900

    デプロイ後の修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,8 +12,8 @@ class Item < ApplicationRecord
     validates :image
   end
 
-  with_options presence: true, format: {with: /\A[0-9]+\d\z/, message: "is invalid"} do
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
+  with_options presence: true, format: {with: /\A[0-9]+\d\z/} do
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   end
 
   validates :category_id, :status_id, :shipping_fee_id, :shipping_area_id, :shipping_date_id, presence: true, numericality: { other_than: 1 }
@@ -21,11 +21,6 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase_record
   has_one_attached :image
-
-  #validates :content, presence: true, unless: :was_attached?
-
-  #def was_attached?
-    #self.image.attached?
-  #end
-
 end
+
+#message: "is invalid"
